2ad0586f9338ec6304813fb823cf8d5892581c3f
James Pacheco <jamesrp13@gmail.com>
2022-04-29 15:39:56 -0600

    Add styling to wallet adapters

--- components/WalletContextProvider.tsx
+++ components/WalletContextProvider.tsx
@@ -4,7 +4,7 @@ import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react
 import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
 import * as web3 from '@solana/web3.js'
 import { PhantomWalletAdapter } from '@solana/wallet-adapter-wallets';
-
+require('@solana/wallet-adapter-react-ui/styles.css')
 
 const WalletContextProvider: FC<{ children: ReactNode }> = ({ children }) => {
     const wallets = [new PhantomWalletAdapter()]
