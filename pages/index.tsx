5815544dea62b4ecea54f94a58718a8ef92f0704
muhammedkurtulus <muhammedkurtulus97@hotmail.com>
2023-01-02 17:30:14 +0300

    ping added

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,31 +1,29 @@
-import { NextPage } from 'next'
-import styles from '../styles/Home.module.css'
-import WalletContextProvider from '../components/WalletContextProvider'
-import { AppBar } from '../components/AppBar'
-import { BalanceDisplay } from '../components/BalanceDisplay'
-import { SendSolForm } from '../components/SendSolForm'
-import Head from 'next/head'
+import { NextPage } from "next";
+import styles from "../styles/Home.module.css";
+import WalletContextProvider from "../components/WalletContextProvider";
+import { AppBar } from "../components/AppBar";
+import { BalanceDisplay } from "../components/BalanceDisplay";
+import { SendSolForm } from "../components/SendSolForm";
+import Head from "next/head";
+import { PingButton } from "../components/PingButton";
 
 const Home: NextPage = (props) => {
-
   return (
     <div className={styles.App}>
       <Head>
         <title>Wallet-Adapter Example</title>
-        <meta
-          name="description"
-          content="Wallet-Adapter Example"
-        />
+        <meta name="description" content="Wallet-Adapter Example" />
       </Head>
       <WalletContextProvider>
         <AppBar />
         <div className={styles.AppBody}>
           <BalanceDisplay />
           <SendSolForm />
+          <PingButton />
         </div>
-      </WalletContextProvider >
+      </WalletContextProvider>
     </div>
   );
-}
+};
 
 export default Home;
